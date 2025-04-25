a687b75839aa815de7013f55cdcd3ade04021139
braIntelligent <m.cataldoolate@gmail.com>
2025-04-25 13:06:14 -0400

    add:blacklistjwt

--- crud_api/urls.py
+++ crud_api/urls.py
@@ -5,12 +5,14 @@ from django.conf.urls.static import static
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenBlacklistView,
 )
 from labels.views import EmailApiView
 
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
     path('admin/', admin.site.urls),
     path('api/', include('labels.urls')),
     path('send-email/', EmailApiView.as_view(), name='send-mail'),
