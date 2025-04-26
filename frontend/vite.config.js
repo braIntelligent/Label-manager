5d0220bb722e5a2b793a421202a79caaae9855fe
braIntelligent <m.cataldoolate@gmail.com>
2025-04-26 16:02:37 -0400

    ready

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '../labels/static/index.html',  // después del build, dónde estará
+          dest: '../../templates'              // destino: BASE_DIR/templates/
+        }
+      ]
+    })
+  ],
   build: {
-    outDir: '../labels/static', 
+    outDir: '../labels/static',
     emptyOutDir: true,
   },
   resolve: {
@@ -13,4 +24,5 @@ export default defineConfig({
       '@': resolve(__dirname, './src'),
     },
   },
+  base: '/static/', // recuerda esto para que busque bien el js/css
 })
