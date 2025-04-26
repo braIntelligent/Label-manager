2814344401805135c331e6a7d063e9a0736c6a26
braIntelligent <m.cataldoolate@gmail.com>
2025-04-26 13:15:02 -0400

    add-fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import PrivateRoute from './components/Layout/PrivateRoute'
-import Login from './pages/Auth/Login'
+import Login from './pages/auth/Login'
 import LabelsPage from './pages/admin/LabelPage'
 import NewLabel from './pages/public/NewLabel';
 import Home from './pages/Home'
